c737137ca75cdb954d800d80bc1119c947e32ad9
duchao3003 <114891424+duchaohachix@users.noreply.github.com>
2022-10-14 14:39:31 +0700

    Linear Regression

---
+++ src/Linear-Regression/cost-linear-reg.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x_train = np.array([1.0, 2.0, 3.0])
+y_train = np.array([300.0, 500.0, 700.0])
+w = 200
+b = 100
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
+
+cost = compute_cost(x_train,y_train,w,b)
+print(f"cost: ${cost}")
---
+++ src/Linear-Regression/sell-house.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x_train = np.array([1.0, 2.0, 3.0])
+y_train = np.array([300.0, 500.0, 700.0])
+w = 100
+b = 100
+def compute_model_output(x, w, b):
+    """
+    Computes the prediction of a linear model
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      w,b (scalar)    : model parameters  
+    Returns
+      y (ndarray (m,)): target values
+    """
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+        
+    return f_wb
+tmp_f_wb = compute_model_output(x_train, w, b)
+
+# Plot our model prediction
+plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
+
