3c0d5d14de8861340adb9d13164bf266bb2e30c7
duchao3003 <114891424+duchaohachix@users.noreply.github.com>
2022-10-21 12:11:58 +0700

    Logistic Regression

---
+++ src/Logistic_Regression/imp_gradient.py
@@ -0,0 +1,174 @@
+import copy, math
+import numpy as np
+from matplotlib import cm
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+from lab_utils_common import plot_data
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_train = np.array([0, 0, 0, 1, 1, 1])
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+         
+    """
+
+    g = 1/(1+np.exp(-z))
+   
+    return g
+def compute_cost_logistic(X, y, w, b):
+    """
+    Computes cost
+
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+             
+    cost = cost / m
+    return cost
+
+def compute_gradient_logistic(X, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+ 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+    Returns
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar)      : The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape
+    dj_dw = np.zeros((n,))                           #(n,)
+    dj_db = 0.
+
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i],w) + b)          #(n,)(n,)=scalar
+        err_i  = f_wb_i  - y[i]                       #scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m                                   #scalar
+        
+    return dj_db, dj_dw  
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    """ truncates color map """
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
+def plt_prob(ax, w_out,b_out):
+    """ plots a decision boundary but include shading to indicate the probability """
+    #setup useful ranges and common linspaces
+    x0_space  = np.linspace(0, 4 , 100)
+    x1_space  = np.linspace(0, 4 , 100)
+
+    # get probability for x0,x1 ranges
+    tmp_x0,tmp_x1 = np.meshgrid(x0_space,x1_space)
+    z = np.zeros_like(tmp_x0)
+    for i in range(tmp_x0.shape[0]):
+        for j in range(tmp_x1.shape[1]):
+            z[i,j] = sigmoid(np.dot(w_out, np.array([tmp_x0[i,j],tmp_x1[i,j]])) + b_out)
+
+
+    cmap = plt.get_cmap('Blues')
+    new_cmap = truncate_colormap(cmap, 0.0, 0.5)
+    pcm = ax.pcolormesh(tmp_x0, tmp_x1, z,
+                   norm=cm.colors.Normalize(vmin=0, vmax=1),
+                   cmap=new_cmap, shading='nearest', alpha = 0.9)
+    ax.figure.colorbar(pcm, ax=ax)
+
+X_tmp = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_tmp = np.array([0, 0, 0, 1, 1, 1])
+w_tmp = np.array([2.,3.])
+b_tmp = 1.
+dj_db_tmp, dj_dw_tmp = compute_gradient_logistic(X_tmp, y_tmp, w_tmp, b_tmp)
+print(f"dj_db: {dj_db_tmp}" )
+print(f"dj_dw: {dj_dw_tmp.tolist()}" )
+
+
+
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters): 
+    """
+    Performs batch gradient descent
+    
+    Args:
+      X (ndarray (m,n)   : Data, m examples with n features
+      y (ndarray (m,))   : target values
+      w_in (ndarray (n,)): Initial values of model parameters  
+      b_in (scalar)      : Initial values of model parameter
+      alpha (float)      : Learning rate
+      num_iters (scalar) : number of iterations to run gradient descent
+      
+    Returns:
+      w (ndarray (n,))   : Updated values of parameters
+      b (scalar)         : Updated value of parameter 
+    """
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+      
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( compute_cost_logistic(X, y, w, b) )
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
+        
+    return w, b, J_history         #return final w,b and J history for graphing
+
+w_tmp  = np.zeros_like(X_train[0])
+b_tmp  = 0.
+alph = 0.1
+iters = 10000
+
+w_out, b_out, _ = gradient_descent(X_train, y_train, w_tmp, b_tmp, alph, iters) 
+print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
+
+
+fig,ax = plt.subplots(1,1,figsize=(5,4))
+# plot the probability 
+plt_prob(ax, w_out, b_out)
+
+# Plot the original data
+ax.set_ylabel(r'$x_1$')
+ax.set_xlabel(r'$x_0$')   
+ax.axis([0, 4, 0, 3.5])
+plot_data(X_train,y_train,ax)
+
+# Plot the decision boundary
+x0 = -b_out/w_out[0]
+x1 = -b_out/w_out[1]
+ax.plot([0,x0],[x1,0], c="b", lw=1)
+plt.show()
